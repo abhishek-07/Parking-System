56313938194bec1e8626b12cbd3874366b0db198
welcome <santhoshnetha99@gmail.com>
2019-03-16 11:59:15 +0530

    pushing first time

---
+++ app/src/main/java/www/santhoshnetha99/com/virtusa/MainActivity.java
@@ -0,0 +1,61 @@
+package www.santhoshnetha99.com.virtusa;
+import www.santhoshnetha99.com.virtusa.parkingdetails.tableschema;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView carno;
+    public TextView intime;
+    public TextView vechiletype;
+
+    Button bt1;
+    private SQLiteDatabase database;
+    private database databaseHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        carno=(TextView)findViewById(R.id.carno);
+        intime=(TextView)findViewById(R.id.intime);
+        vechiletype=(TextView)findViewById(R.id.vechiletype);
+
+        bt1=(Button)findViewById(R.id.bt1);
+        databaseHelper = new database(MainActivity.this);
+        database = databaseHelper.getWritableDatabase();
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int spacetaken;
+                String carno1 	= carno.getText().toString();
+                String intime1= intime.getText().toString();
+                int vechiletype1=Integer.parseInt(vechiletype.getText().toString());
+                if (vechiletype1==4)
+                    spacetaken=1;
+                else
+                    spacetaken=4;
+
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(tableschema._ID, carno1);
+                contentValues.put(tableschema.IN_TIME, intime1);
+                contentValues.put(tableschema.VECHILE_TYPE, vechiletype1);
+                contentValues.put(tableschema.SPACE_TAKEN, spacetaken);
+
+
+                long rowId = database.insert(tableschema.TABLE_NAME,null, contentValues);
+
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/www/santhoshnetha99/com/virtusa/database.java
@@ -0,0 +1,35 @@
+package www.santhoshnetha99.com.virtusa;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import www.santhoshnetha99.com.virtusa.parkingdetails.tableschema;
+
+public class database extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "parkingdetails.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private final String SQL_CREATE_TABLE
+            = "CREATE TABLE " + tableschema.TABLE_NAME
+            + " (" + tableschema._ID + " INTEGER PRIMARY KEY , "
+            + tableschema.IN_TIME + " TEXT NOT NULL, "
+            + tableschema.VECHILE_TYPE + " INTEGER NOT NULL, "
+            + tableschema.SPACE_TAKEN+" INTEGER NOT NULL "+");";
+
+    public database(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Ideally we wouldn't want to delete all of our entries!
+        db.execSQL("DROP TABLE IF EXISTS " + tableschema.TABLE_NAME);
+        onCreate(db);	// Call to create a new db with upgraded schema and version
+    }
+}
