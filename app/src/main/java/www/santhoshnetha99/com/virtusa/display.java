7e6468091ef6c5ebcf1a318159eb3dd91485d213
Abhishek Reddy <abhishek.police1@gmail.com>
2019-03-16 14:02:26 +0530

    base commit for database!

--- app/src/main/java/www/santhoshnetha99/com/virtusa/display.java
+++ app/src/main/java/www/santhoshnetha99/com/virtusa/display.java
@@ -23,7 +23,7 @@ public class display extends AppCompatActivity {
 
 class CustomAdapter extends BaseAdapter {
     private Context mContext;
-    database controld;
+    database controld1;
     SQLiteDatabase db;
     private ArrayList<String> _ID = new ArrayList<String>();
     private ArrayList<String> IN_TIME = new ArrayList<String>();
